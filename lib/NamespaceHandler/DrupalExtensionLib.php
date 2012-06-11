d8ca8f45ffdc8777dd936b59f68e7de7034f0687
Andreas Hennings <andreas@sharkysoup.com>
2012-06-11 05:11:56 +0200

    allow for underscores in lowercase module names

--- lib/NamespaceHandler/DrupalExtensionLib.php
+++ lib/NamespaceHandler/DrupalExtensionLib.php
@@ -15,22 +15,40 @@ class xautoload_NamespaceHandler_DrupalExtensionLib implements xautoload_Namespa
    * $psr_0_root . '/' . $path_prefix_symbolic . $path_suffix
    */
   function findFile($api, $path_prefix_symbolic, $path_suffix) {
-    if (FALSE !== $pos = strpos($path_suffix, DIRECTORY_SEPARATOR)) {
-      $module = substr($path_suffix, 0, $pos);
-      if (!isset($this->modules[$module])) {
-        if ($this->system->module_exists($module)) {
-          $module_dir = $this->system->drupal_get_path('module', $module);
-          $this->modules[$module] = $this->_moduleClassesDir($module, $module_dir, $path_prefix_symbolic);
-        }
-        else {
-          $this->modules[$module] = FALSE;
-        }
+    $pos = 0;
+    while (TRUE) {
+      $pos = strpos($path_suffix, DIRECTORY_SEPARATOR);
+      if (FALSE === $pos) {
+        return FALSE;
       }
+      $char = @$path_suffix{$pos + 1};
+      if (isset($char) && "$char" === strtoupper($char)) {
+        // We found a '_' followed by an uppercase character.
+        break;
+      }
+      // We hit a normal '_' within a module name.
+      $path_suffix[$pos] = '_';
+    }
+    if (FALSE !== $pos) {
+      $module = substr($path_suffix, 0, $pos);
+      $this->_initModule($module, $path_prefix_symbolic);
       if (!empty($this->modules[$module])) {
         $path = $this->modules[$module] . substr($path_suffix, $pos + 1);
         if ($api->suggestFile($path)) {
           return TRUE;
         }
+      }
+    }
+  }
+
+  protected function _initModule($module, $path_prefix_symbolic) {
+    if (!isset($this->modules[$module])) {
+      if ($this->system->module_exists($module)) {
+        $module_dir = $this->system->drupal_get_path('module', $module);
+        $this->modules[$module] = $this->_moduleClassesDir($module, $module_dir, $path_prefix_symbolic);
+      }
+      else {
+        $this->modules[$module] = FALSE;
       }
     }
   }
