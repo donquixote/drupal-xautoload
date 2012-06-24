bce61b7f2ed8828c9073cded5427ac1766990fb3
Andreas Hennings <andreas@sharkysoup.com>
2012-06-24 16:05:13 +0200

    change everything to extension instead of module. Thanks indytechcook. See http://drupal.org/node/1653062#comment-6151430

--- lib/NamespaceHandler/DrupalExtensionPSR0.php
+++ lib/NamespaceHandler/DrupalExtensionPSR0.php
@@ -3,8 +3,8 @@
 
 class xautoload_NamespaceHandler_DrupalExtensionPSR0 extends xautoload_NamespaceHandler_DrupalExtensionLib {
 
-  protected function _moduleClassesDir($module, $module_dir, $path_prefix_symbolic) {
-    return $module_dir . DIRECTORY_SEPARATOR .
-      $path_prefix_symbolic . $module . DIRECTORY_SEPARATOR;
+  protected function _extensionClassesDir($name, $extension_dir, $path_prefix_symbolic) {
+    return $extension_dir . DIRECTORY_SEPARATOR .
+      $path_prefix_symbolic . $name . DIRECTORY_SEPARATOR;
   }
 }
